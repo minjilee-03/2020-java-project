545e4122071608f0e0abd6ab01361b0a7048361a
minjilee-03 <56942604+minjilee-03@users.noreply.github.com>
2020-11-14 13:38:00 +0900

    스테이지 시작전 select item

--- SelectItem.java
+++ SelectItem.java
@@ -17,14 +17,6 @@ public class SelectItem extends JFrame implements ActionListener {
 	Music introMusic = new Music("..\\music\\ITEM_BGM.MP3", true);//뮤직
 	public SelectItem() {
 		getContentPane().setLayout(null);
-		
-		btn = new JButton();
-		btn.setOpaque(false);
-		btn.setBorderPainted(false);
-		btn.setBounds(0, 0, 33, 35);
-		getContentPane().add(btn);
-		btn.addActionListener(this);
-		btn.doClick();
 		JPanel p = new JPanel();
 		p.setBounds(0, 0, 0, 0);
 		JButton backbtn = new JButton(""); 
@@ -32,6 +24,8 @@ public class SelectItem extends JFrame implements ActionListener {
 		getContentPane().add(p);
 		p.setLayout(null);
 		p.add(backbtn);
+		
+
 		
 		backbtn.setBounds(0, 743, 196, 51); 
 	
@@ -46,6 +40,15 @@ public class SelectItem extends JFrame implements ActionListener {
 					
 			}
 		});
+		
+		JButton button_1 = new JButton("");
+		button_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		button_1.setIcon(new ImageIcon("..\\image\\back_btn.jpg"));
+		button_1.setBounds(0, 753, 166, 41);
+		getContentPane().add(button_1);
 		button.setIcon(new ImageIcon("..\\image\\start_game.jpg"));
 		button.setBounds(700, 541, 652, 195);
 		getContentPane().add(button);
@@ -54,6 +57,15 @@ public class SelectItem extends JFrame implements ActionListener {
 		label_1.setIcon(new ImageIcon("..\\image\\Item_back.jpg"));
 		label_1.setBounds(0, 0, 1478, 794);
 		getContentPane().add(label_1);
+		
+		
+		btn = new JButton();
+		btn.setOpaque(false);
+		btn.setBorderPainted(false);
+		btn.setBounds(0, 0, 0, 9);
+		getContentPane().add(btn);
+		btn.addActionListener(this);
+		btn.doClick();
 	
 		setTitle("SchoolRun!");
 		setLocation(0, 0);
@@ -66,8 +78,8 @@ public class SelectItem extends JFrame implements ActionListener {
 			public void actionPerformed(ActionEvent e) {
 				introMusic.close();
 				setVisible(false);
-				Main_start ms = new Main_start(); //캐릭터, 테마 중에 무엇을 고를지 선택
-				ms.main(null);
+				Main_lobi ms = new Main_lobi(); //캐릭터, 테마 중에 무엇을 고를지 선택
+				ms.setVisible(true);;
 			}
 		});
 		
